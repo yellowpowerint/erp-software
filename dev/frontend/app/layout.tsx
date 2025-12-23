5bc6f125e084fadd2221b2b2d903df48c95247f7
webblabsorg <webblabsorg@gmail.com>
2025-12-23 10:51:51 +0000

    chore(branding): add favicon

--- dev/frontend/app/layout.tsx
+++ dev/frontend/app/layout.tsx
@@ -8,6 +8,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.svg', type: 'image/svg+xml' },
+      { url: '/favicon.png', type: 'image/png' },
       { url: '/favicon.jpg', type: 'image/jpeg' },
     ],
   },
