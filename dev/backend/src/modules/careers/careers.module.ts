270efc973bf941effd954b74fd568dcc3f9a6e01
webblabsorg <webblabsorg@gmail.com>
2025-12-31 22:50:37 +0000

    fix(careers): correct Prisma import paths to use common/prisma

--- dev/backend/src/modules/careers/careers.module.ts
+++ dev/backend/src/modules/careers/careers.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CareersController } from './careers.controller';
 import { CareersService } from './careers.service';
-import { PrismaModule } from '../prisma/prisma.module';
+import { PrismaModule } from '../../common/prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
